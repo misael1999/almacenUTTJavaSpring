f9ebb00770c7232223a1e11c3eda9262cea93891
misael ivan gonzalez uscanga <misael.gonzalez@alumno.uttehuacan.edu.mx>
2018-10-24 16:20:13 -0500

    Configuracion de spring security y creacion de entidades usuario y area

--- src/main/java/com/flamel/almacenutt/AlmacenuttApplication.java
+++ src/main/java/com/flamel/almacenutt/AlmacenuttApplication.java
@@ -1,12 +1,19 @@
 package com.flamel.almacenutt;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AlmacenuttApplication {
+public class AlmacenuttApplication implements CommandLineRunner {
+
 
     public static void main(String[] args) {
         SpringApplication.run(AlmacenuttApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
     }
 }
